9f81bf380b961a70b9700ad7132143a7c15f2e30
LaurenAnds <laurenjayne.ands@outlook.com>
2023-12-13 15:54:54 +0000

    my sketch initial commit

--- js/mySketch.js
+++ js/mySketch.js
@@ -2,29 +2,44 @@
 //It must be loaded from index.html
 //It assumes that the file "myPalettes.js" has also been loaded
 
-let currentPalette;
+let x, y;
+let h, w;
 
+//This "setup" function is called once, at start
 function setup() {
-    createCanvas(windowWidth, windowHeight / 2);
-    currentPalette = randomPalette();
-    noStroke();
-    background("white");
+	createCanvas(windowWidth/2, windowHeight/2); // width and height, in pixels
+	background('gray');
 }
 
+
+
+// We put our drawing commands in this "draw" function.
+// It will be called automatically, up to 60 times per second!
 function draw() {
-    fill(random(currentPalette));
-    const x = random(0, width);
-    const y = random(0, height);
-    circle(x, y, 100);
-}
+	x = random(windowWidth)
+	y = random(windowHeight)
+	h = random(100)
+	w = random(100)
+	
+if(y < 300)
+		fill("orange")
+	
+if(y > 300)
+	fill("skyblue")
+	
 
-function mouseClicked() {
-    background(255);
-    currentPalette = randomPalette();
-}
-
-function keyPressed() {
-    if (key === "s") {
-        save("my-p5-screenshot");
-    }
+	noStroke() // Colour-in the next shapes, this colour, please!
+	// noStroke();  // Don't outline the next shapes, please.
+	
+	//Draw a rectangle positioned at x: 200, y: 100,
+	//and with a width of 300 and height of 150
+	rect(x, y, w, h, 20, 20);
+	
+	//draw another rectangle elsewhere
+	rect(x, y, w, h, 20, 20);
+	
+	if (frameCount > 300) 
+	noLoop();
+	
+	
 }
